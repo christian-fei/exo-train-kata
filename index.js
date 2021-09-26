7fa747a3fda8aa6c5584584e19213ecf7ad16909
Christian Fei <crifei93@gmail.com>
2021-09-26 11:41:29 +0200

    lint

--- index.js
+++ index.js
@@ -3,33 +3,32 @@ const REVERSED_LOCOMOTIVE = 'HHHH>'
 const PASSENGER_CAR = '|OOOO|'
 const RESTAURANT_CAR = '|hThT|'
 const REPRESENTATIONS = {
-    H: REVERSED_LOCOMOTIVE,
-    P: PASSENGER_CAR,
-    R: RESTAURANT_CAR
+  H: REVERSED_LOCOMOTIVE,
+  P: PASSENGER_CAR,
+  R: RESTAURANT_CAR
 }
 
 module.exports = class Train {
-    constructor(trainRepresentation = '') {
-        this.trainRepresentation = trainRepresentation
-    }
+  constructor (trainRepresentation = '') {
+    this.trainRepresentation = trainRepresentation
+  }
 
-    toASCII() {
-        return this.trainRepresentation
-            .split('')
-            .map((car, index) => {
-                const isHead = (index === 0)
-                if (car === 'H' && isHead) return LOCOMOTIVE
-                return REPRESENTATIONS[car]
-            })
-            .join('::')
+  toASCII () {
+    return this.trainRepresentation
+      .split('')
+      .map((car, index) => {
+        const isHead = (index === 0)
+        if (car === 'H' && isHead) return LOCOMOTIVE
+        return REPRESENTATIONS[car]
+      })
+      .join('::')
+  }
 
-    }
+  detachHead () {
+    this.trainRepresentation = this.trainRepresentation.substring(1, this.trainRepresentation.length)
+  }
 
-    detachHead () {
-        this.trainRepresentation = this.trainRepresentation.substring(1, this.trainRepresentation.length)
-    }
-
-    detachEnd () {
-        this.trainRepresentation = this.trainRepresentation.substring(0, this.trainRepresentation.length - 1)
-    }
+  detachEnd () {
+    this.trainRepresentation = this.trainRepresentation.substring(0, this.trainRepresentation.length - 1)
+  }
 }
--- index.test.js
+++ index.test.js
@@ -3,34 +3,34 @@ const Train = require('.')
 // const trainToASCII = require('.')
 
 test('empty input', t => {
-    const train = new Train('')
-    t.is(train.toASCII(), '')
+  const train = new Train('')
+  t.is(train.toASCII(), '')
 })
 
 test('input locomotive, "H"', t => {
-    const train = new Train('H')
-    t.is(train.toASCII(), '<HHHH')
+  const train = new Train('H')
+  t.is(train.toASCII(), '<HHHH')
 })
 
 test('input locomotive and passenger car, "HP"', t => {
-    const train = new Train('HP')
-    t.is(train.toASCII(), '<HHHH::|OOOO|')
+  const train = new Train('HP')
+  t.is(train.toASCII(), '<HHHH::|OOOO|')
 })
 
 test('input locomotive and two passenger cars, "HPP"', t => {
-    const train = new Train('HPP')
-    t.is(train.toASCII(), '<HHHH::|OOOO|::|OOOO|')
+  const train = new Train('HPP')
+  t.is(train.toASCII(), '<HHHH::|OOOO|::|OOOO|')
 })
 
 test('locomotive at the end, "HPRPH"', t => {
-    const train = new Train('HPRPH')
-    t.is(train.toASCII(), '<HHHH::|OOOO|::|hThT|::|OOOO|::HHHH>')
+  const train = new Train('HPRPH')
+  t.is(train.toASCII(), '<HHHH::|OOOO|::|hThT|::|OOOO|::HHHH>')
 })
 
 test('detach head and tail of train, "HPRPH" -> "HPRP"', t => {
-    const train = new Train('HPRPH')
-    train.detachEnd()
-    t.is(train.toASCII(), '<HHHH::|OOOO|::|hThT|::|OOOO|')
-    train.detachHead()
-    t.is(train.toASCII(), '|OOOO|::|hThT|::|OOOO|')
+  const train = new Train('HPRPH')
+  train.detachEnd()
+  t.is(train.toASCII(), '<HHHH::|OOOO|::|hThT|::|OOOO|')
+  train.detachHead()
+  t.is(train.toASCII(), '|OOOO|::|hThT|::|OOOO|')
 })
